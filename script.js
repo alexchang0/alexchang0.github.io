2bf9a48f26cdb9ceb1ff8d9c71a561ec44143d62
alexchang0 <154647691+alexchang0@users.noreply.github.com>
2025-03-31 15:27:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.addEventListener("mousemove", (e) => {
-    let sparkle = document.createElement("div");
-    sparkle.classList.add("sparkle");
+document.body.addEventListener('mousemove', (e) => {
+    const sparkle = document.createElement('div');
+    sparkle.classList.add('sparkle');
     document.body.appendChild(sparkle);
+    sparkle.style.left = `${e.pageX - 3}px`; // Adjust for center of sparkle
+    sparkle.style.top = `${e.pageY - 3}px`; // Adjust for center of sparkle
 
-    sparkle.style.left = `${e.clientX}px`;
-    sparkle.style.top = `${e.clientY}px`;
-
+    // Remove the sparkle after the animation is done
     setTimeout(() => {
         sparkle.remove();
-    }, 500); // Remove after animation
+    }, 500); // Duration of the animation
 });
